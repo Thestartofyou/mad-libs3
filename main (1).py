8de6b90aae42dfb624cc31a280897a0db2d34075
Thestartofyou <38260499+Thestartofyou@users.noreply.github.com>
2023-03-19 08:52:04 -0400

    mad libs3
    no existing changes

---
+++ main (1).py
@@ -0,0 +1,27 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# Mad Libs Game in Python
+
+print("Welcome to Mad Libs!")
+
+# Ask the user for inputs
+noun1 = input("Enter a noun: ")
+adjective1 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+noun2 = input("Enter another noun: ")
+place1 = input("Enter a place: ")
+verb2 = input("Enter another verb: ")
+adjective2 = input("Enter another adjective: ")
+
+# Print the story with user inputs
+print(f"Once upon a time, there was a {noun1} that was very {adjective1}.")
+print(f"It loved to {verb1} with its best friend, a {noun2} that lived in {place1}.")
+print(f"They would always {verb2} together and have a {adjective2} time.")
+print("The end!")
+
